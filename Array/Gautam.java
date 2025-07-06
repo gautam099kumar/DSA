6c55c535f821cbd45c2a489ef89831f8070aab45
gautam099kumar <gautam099kumar@gmail.com>
2025-07-06 19:52:47 +0530

    stage1

--- Array/Gautam.java
+++ Array/Gautam.java
@@ -1,3 +1,4 @@
+package Array;
 public class Gautam{
     public static void main(String args[]){
         System.out.println("Gautam");
--- Array/ques2.java
+++ Array/ques2.java
@@ -1,3 +1,4 @@
+package Array;
 public class ques2 {
     public static void main(String[] args) {
         System.out.println("ques 2");
